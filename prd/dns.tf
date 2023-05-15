12a9e2d4258d5b76dfab8843955df268b12cd7fb
Saravanakumar Gurusamy <31770496+SaravananGuru@users.noreply.github.com>
2023-05-15 19:37:35 +0530

    Add files via upload

---
+++ prd/dns.tf
@@ -0,0 +1,9 @@
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.az_rg.name
+}
+
+resource "azurerm_private_dns_zone" "dfs" {
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = azurerm_resource_group.az_rg.name
+}
