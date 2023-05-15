6461a6d0415d8eb41f0a817e9bcd77e896980114
Saravanakumar Gurusamy <31770496+SaravananGuru@users.noreply.github.com>
2023-05-15 15:11:32 +0530

    local value

--- sbx/storage.tf
+++ sbx/storage.tf
@@ -15,6 +15,36 @@ provider "azurerm" {
   features {}
 }
 
+# Local value declaration
+
+locals {
+  storage_account_suffix = [
+    {
+      account_name                  = "stprodshpequip"
+      account_tier                  = "Standard"
+      adls_replication              = "LRS"
+      containers                    = "raw,stage,analytic,hubscan"
+      file_shares                   = ""
+      is_hns_enabled                = "true"
+      is_private_end_point_required = "false"
+      queues                        = ""
+      eai                           = "3535162"
+    }]
+
+    private_endpoints = [{
+      name                 = "stprdshpequipblobst"
+      storage_account      = "stprodshpequip"
+      subnet_id            = "/subscriptions/aa365c78-baef-4db6-9b9a-682db6e1de6d/resourceGroups/centralus-fxe-data-mgmt-prd-network-rg/providers/Microsoft.Network/virtualNetworks/centralus-fxe-data-mgmt-prd-fxconnect-vnet/subnets/datalake"
+      subresource_name     = "blob"
+      zone_name            = "privatelink.blob.core.windows.net"
+      private_dns_zone_id  = "/subscriptions/aa365c78-baef-4db6-9b9a-682db6e1de6d/resourceGroups/centralus-fxe-data-mgmt-prd-private-dns-rg/providers/Microsoft.Network/privateDnsZones/privatelink.blob.core.windows.net"
+      is_manual_connection = false
+      virtual_network_subnets = ["/subscriptions/aa365c78-baef-4db6-9b9a-682db6e1de6d/resourceGroups/centralus-fxe-data-mgmt-prd-network-rg/providers/Microsoft.Network/virtualNetworks/centralus-fxe-data-mgmt-prd-main-vnet/subnets/devops-agents", "/subscriptions/aa365c78-baef-4db6-9b9a-682db6e1de6d/resourceGroups/centralus-fxe-data-mgmt-prd-network-rg/providers/Microsoft.Network/virtualNetworks/centralus-fxe-data-mgmt-prd-fxconnect-vnet/subnets/datalake"]
+      allow_subnet_rule = false 
+      allow_pep = true
+    }]
+}
+
 # storage account creation
 
 module "DataLakeStorage" {
