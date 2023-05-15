dbf720f4c99aa1ecc0d4d339ec4e61863a1a45f3
Saravanakumar Gurusamy <31770496+SaravananGuru@users.noreply.github.com>
2023-05-15 15:03:41 +0530

    storage ac

--- npe/storage.tf
+++ npe/storage.tf
@@ -14,3 +14,18 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+# storage account creation
+
+module "DataLakeStorage" {
+  source                       = "git::git@ssh.dev.azure.com:v3/fxe-data-mgmt/common/terraform-azurerm-auxiliaryDataLakeStorage?ref=main"
+  storage_account_list       = local.storage_account_list
+  environment_name           = local.environment_name
+  location                   = local.location
+  resource_group_name        = azurerm_resource_group.this["primary"].name
+  subnet_id                  = local.subnet_id
+  subresource_names          = local.subresource_names
+  private_endpoints          = local.private_endpoints
+  tags                       = local.common_dmo_tags
+  app_tags                   = local.pip_tags
+}
