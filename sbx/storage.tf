cde67e3f2b8cfa0fc990d079c6a936ebcd19b334
Saravanakumar Gurusamy <31770496+SaravananGuru@users.noreply.github.com>
2023-05-15 14:11:15 +0530

    azurerm prov add

--- sbx/storage.tf
+++ sbx/storage.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.54.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=1.6.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
